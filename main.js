4735b6e99d8694c9b3dfa9cbc981b0ebccd96e08
Cleveland Pitt Jr <clevelandpittjr@Clevelands-MBP.fios-router.home>
2019-11-02 18:47:59 -0400

    added some style

--- main.js
+++ main.js
@@ -17,14 +17,17 @@ else if(characters < 8){
 function generate(){
 
     let complexity = document.getElementById("slider").value;
+
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+
     let password = "";
+
     
-    for (var i = 0; i <= complexity; i++) {
+    for(var i = 0; i <= complexity; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
     
-    document.getElementById("display").values = password;
+    document.getElementById("display").value = password;
 
 
 }
