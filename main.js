c9a0ba16110df26ced51d68f9777dfbad2e0af82
Cleveland Pitt Jr <clevelandpittjr@Clevelands-MBP.fios-router.home>
2019-11-02 01:15:37 -0400

    add css and js

---
+++ main.js
@@ -0,0 +1,11 @@
+function generate(){
+
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+    let password = "";
+    
+    for (var i = 0; i <= complexity; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
+    }
+    
+    document.getElementById("display").value = password;
+}
