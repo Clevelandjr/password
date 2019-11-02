d012432b3509623320891098eb47eef87cb0f452
Cleveland Pitt Jr <clevelandpittjr@Clevelands-MBP.fios-router.home>
2019-11-02 03:54:36 -0400

    added prompts and alerts

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+var creatPassword = confirm("Want to create a Password?");
+if(creatPassword){
+    alert("Great! You came to the right site!")
+}
+else{
+    alert("This isn't the page for you.")
+}
+var characters = prompt("How many charcters?");
+if(characters > 7){
+    alert("We can help you!")
+}
+else if(characters < 8){
+    alert("We need at least 8 characters.")
+}
+
+
 function generate(){
 
     let complexity = document.getElementById("slider").value;
@@ -5,8 +21,10 @@ function generate(){
     let password = "";
     
     for (var i = 0; i <= complexity; i++) {
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
     
-    document.getElementById("display").value = password;
+    document.getElementById("display").values = password;
+
+
 }
